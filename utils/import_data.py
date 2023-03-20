1af4f2eaa4427802db9cc73d320c0701983677f3
EF <efraisma.ef7@gmail.com>
2023-03-20 12:50:08 -0600

    Update import_data.py
    Modify GSheets

--- utils/import_data.py
+++ utils/import_data.py
@@ -1,10 +1,30 @@
 from os import getenv
-from gspread import authorize
 from dotenv import load_dotenv
 from pandas import DataFrame, concat
+from gspread import Worksheet, authorize
 from gspread.exceptions import WorksheetNotFound
 from oauth2client.service_account import ServiceAccountCredentials as sac
 
+load_dotenv()
+GOOGLE_SHEETS_CREDENTIALS = {
+        "type": "service_account",
+        "project_id": "gymworkout",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "private_key_id": getenv("GOOGLE_PRIVATE_KEY_ID"),
+        "private_key": getenv("GOOGLE_PRIVATE_KEY").replace("\\n", "\n"),
+        "client_email": getenv("GOOGLE_CLIENT_EMAIL"),
+        "client_id": getenv("GOOGLE_CLIENT_ID"),
+        "client_x509_cert_url": getenv("GOOGLE_CLIENT_X509_CERT_URL"),
+    }
+
+def connect_gsheets(gsheets_url: str, credentials: dict) -> Worksheet:
+    FEEDS = "https://spreadsheets.google.com/feeds"
+    client = sac.from_json_keyfile_dict(credentials, FEEDS)
+    session = authorize(client)
+    book = session.open_by_url(gsheets_url)
+    return book
 
 def read_gsheets(gsheets_url: str, sheets_to_read: list) -> DataFrame:
     """
@@ -18,26 +38,7 @@ def read_gsheets(gsheets_url: str, sheets_to_read: list) -> DataFrame:
         DataFrame, append of all sheets specified from Google Sheets
     """
 
-    load_dotenv()
-
-    AUTH_PROVIDER = "https://www.googleapis.com/oauth2/v1/certs"
-    cred = {
-        "type": "service_account",
-        "project_id": "gymworkout",
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": AUTH_PROVIDER,
-        "private_key_id": getenv("GOOGLE_PRIVATE_KEY_ID"),
-        "private_key": getenv("GOOGLE_PRIVATE_KEY").replace("\\n", "\n"),
-        "client_email": getenv("GOOGLE_CLIENT_EMAIL"),
-        "client_id": getenv("GOOGLE_CLIENT_ID"),
-        "client_x509_cert_url": getenv("GOOGLE_CLIENT_X509_CERT_URL"),
-    }
-
-    FEEDS = "https://spreadsheets.google.com/feeds"
-    client = sac.from_json_keyfile_dict(cred, FEEDS)
-    session = authorize(client)
-    book = session.open_by_url(gsheets_url)
+    book = connect_gsheets(gsheets_url, GOOGLE_SHEETS_CREDENTIALS)
 
     df = DataFrame()
     for sheet in sheets_to_read:
@@ -53,3 +54,13 @@ def read_gsheets(gsheets_url: str, sheets_to_read: list) -> DataFrame:
         except WorksheetNotFound:
             print(f'Sheetname: "{sheet}" was not found at\n{gsheets_url}')
     return df
+
+def update_sheet(gsheets_url: str, sheet_name: str, data: DataFrame) -> None:
+    book = connect_gsheets(gsheets_url, GOOGLE_SHEETS_CREDENTIALS)
+    
+    try:
+        sheet = book.worksheet(sheet_name)
+    except WorksheetNotFound:
+        print(f'Sheetname: "{sheet}" was not found at\n{gsheets_url}')
+
+    sheet.update([data.columns.values.tolist()] + data.values.tolist())
